d7f4705fb77e0b066e9630aa4100030e3b6500d4
Pizzzo <chevezpeter@gmail.com>
2026-02-06 13:32:41 -0800

    Setting up main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
+import './styles.css';
+import { createApp } from './app/app.js';
 
+// NOTE: (peter) - Mount the app to main div element.
+const mount = document.querySelector('#app');
+// NOTE: (peter) - setup router global.
+const router = createApp({ mount });
+
+router.start();
