ab75ac6fc3319371ff49a7d3e7acdbf6089c5891
Pizzzo <chevezpeter@gmail.com>
2026-02-06 12:14:37 -0800

    added tailwindcss, continuing with layout.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Store from './services/Store.js';
 import API from './services/API.js';
 import { loadData } from './services/Events.js';
+import Router from './services/Router.js';
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
-window.addEventListener('DOMContentLoaded', async () => {
-    await loadData();
-});
+const sidebar = document.querySelector('.sidebar-menu');
+sidebar.classList.add('-translate-x-full');
+
+console.log(sidebar);
