32f4c9a5d5b3081d3562621f70a31b7f04306401
Pizzzo <chevezpeter@gmail.com>
2026-02-13 14:31:43 -0800

    Testing out theme toggle button on main header. Updated CSS to make use of more of my custom tokens, helping with modularity. Set themes for lightmode/darkmode should persist after reload.

--- src/app.js
+++ src/app.js
@@ -15,18 +15,39 @@ app.router = Router;
 //NOTE: (peter) - exposing to window for console testing.
 app.store = store;
 app.api = api;
+const body = document.body;
+const menuBtn = document.querySelector('.menu-button');
+const themeBtn = document.getElementById('theme-toggle');
+const backdrop = document.querySelector('[data-backdrop]');
+
+function setTheme() {
+    if (body.classList.contains('theme-light')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        body.classList.remove('theme-light');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+function initTheme() {
+    const savedTheme = localStorage.getItem('theme');
+    console.log(savedTheme);
+    if (savedTheme === 'light') {
+        body.classList.add('theme-light');
+    }
+    setTheme(savedTheme || 'dark');
+}
+
+initTheme();
 
 window.addEventListener('DOMContentLoaded', () => {
     loadEvents();
     //// NOTE: (peter) - app starts here.
     app.router.init();
 
-    const menuBtn = document.querySelector('.menu-button');
-    const backdrop = document.querySelector('[data-backdrop]');
-
     // NOTE: (peter) - logic for nav button.
     function setMenuOpen(isOpen) {
-        document.body.dataset.menuOpen = String(isOpen);
+        body.dataset.menuOpen = String(isOpen);
         if (backdrop) backdrop.hidden = !isOpen;
         if (menuBtn) menuBtn.setAttribute('aria-expanded', String(isOpen));
     }
@@ -35,8 +56,14 @@ window.addEventListener('DOMContentLoaded', () => {
     app.setMenuOpen = setMenuOpen;
 
     menuBtn?.addEventListener('click', () => {
-        const open = document.body.dataset.menuOpen === 'true';
+        const open = body.dataset.menuOpen === 'true';
         setMenuOpen(!open);
+    });
+
+    themeBtn?.addEventListener('click', () => {
+        body.classList.toggle('theme-light');
+        console.log(body.classList.contains('theme-light'));
+        setTheme();
     });
 
     backdrop?.addEventListener('click', () => setMenuOpen(false));
@@ -45,3 +72,5 @@ window.addEventListener('DOMContentLoaded', () => {
         if (e.key === 'Escape') setMenuOpen(false);
     });
 });
+
+const toggleBtn = document.getElementById('toggle-mode');
